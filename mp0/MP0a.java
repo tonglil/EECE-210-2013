463cd651db586e399cae2506b2ec2c9ec905e4e5
Sathish Gopalakrishnan <sathish@ece.ubc.ca>
2013-09-04 03:22:37 -0700

    Files were renamed.

---
+++ mp0/MP0a.java
@@ -0,0 +1,27 @@
+/**
+ * @author Sathish Gopalakrishnan
+ *
+ */
+public class Lab0a {
+	static public void main( String[] args ) {
+		int n = 1001;
+		
+		if ( n < 1 ) {
+			System.out.println( "Incorrect input. Terminating program." );
+			return;
+		}
+		
+		while ( n > 1 ) {
+			System.out.print( n + " ");
+			if ( n%2 == 0 ) {
+				n = n/2;
+			}
+			else {
+				n = 3*n + 1;
+			}
+		}
+		
+		System.out.println( n );
+		
+	}
+}
