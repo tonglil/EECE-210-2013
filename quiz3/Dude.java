27601d997f3844769927d1b0e6193569e4f1d1bc
Sathish Gopalakrishnan <sathish@Ubuntu1204-64bit.(none)>
2013-09-17 09:49:18 -0700

    Updated Dude.java

--- quiz3/Dude.java
+++ quiz3/Dude.java
@@ -1,3 +1,11 @@
+/*
+	The purpose of this example program is to illustrate several aspects of Java.
+	Some of these aspects were not emphasized in other notes (e.g., nested classes).
+	You may want to read more about some of these aspects on your own.
+	Note the use of static fields, as well as public and private fields.
+	You can experiment with the code snippets to get a better understanding of Java.
+*/
+
 public class Dude {
 	private String name;
 	private int hp;
@@ -26,7 +34,7 @@ public class Dude {
 		Dude d2 = new Dude( "Bilbo" );
 		Dude d3 = d2;
 		
-		Armour a = new Armour( );			// line a
+		// Armour a = new Armour( );			// line a
 		Armour b = d3.new Armour( );			// line b
 		System.out.println( Dude.getNumDudes( ) );	// line c
 		System.out.println( d3.getNumDudes( ) );	// line d
