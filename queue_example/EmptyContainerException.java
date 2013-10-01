6c7020a30ea2f96960353c8724ef77acffe005c5
EECE 210 User <eece210@eece210vm.(none)>
2013-10-01 11:32:21 -0700

    Additional comment for EmptyContainerException

--- queue_example/EmptyContainerException.java
+++ queue_example/EmptyContainerException.java
@@ -8,4 +8,6 @@
 public class EmptyContainerException extends Exception {
 	// we are not doing anything interesting here
 	// although we could choose to do so.
+	// This should be a checked exception so we extend
+	// Exception.
 }
