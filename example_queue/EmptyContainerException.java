bd1c1f9274f3b575753e30933af66446eb42ef86
EECE 210 User <eece210@eece210vm.(none)>
2013-10-01 10:41:21 -0700

    Added example for creating a Queue ADT

---
+++ example_queue/EmptyContainerException.java
@@ -0,0 +1,11 @@
+/**
+ * 
+ * @author Sathish Gopalakrishnan
+ * This class is for a simple exception to be used when
+ * an empty container is accessed and this is inappropriate.
+ *
+ */
+public class EmptyContainerException extends Exception {
+	// we are not doing anything interesting here
+	// although we could choose to do so.
+}
