f62b74ed5326a19a0ebf831d186df799d1a168df
Tony Li <tonyli.lives@gmail.com>
2013-10-30 01:51:17 -0700

    tests use built in asserts

--- Assignments/Assignment 3/dir/tests/org/mozilla/javascript/TokenStreamTest.java
+++ Assignments/Assignment 3/dir/tests/org/mozilla/javascript/TokenStreamTest.java
@@ -90,7 +90,7 @@ public class TokenStreamTest {
         try {
             //reflectAll(ts);
             assertTrue(reflectProperty(ts, "sourceString") == "abc");
-            assertTrue(reflectProperty(ts, "sourceReader") == null);
+            assertNull(reflectProperty(ts, "sourceReader"));
             assertTrue(reflectPropertyInt(ts, "sourceEnd") >= 0);
             assertTrue(reflectPropertyInt(ts, "sourceCursor") == 0);
             assertTrue(reflectPropertyInt(ts, "cursor") == 0);
@@ -101,31 +101,19 @@ public class TokenStreamTest {
     }
 
     @Test
-    public void testTokenStreamNullString() {
-        // Tests the constructor function
-        // Input: non-null sourceReader, null sourceString
+    public void testTokenStreamNullStringJS() {
+        // Tests the constructor function with .js
+        // Input: sourceReader with javascript, null sourceString
         // Expected output:
         // (this.sourceString == null ==> this.sourceReader != null)
         // (this.sourceEnd >= 0)
         // (this.sourceCursor == 0 && this.cursor == 0);
 
-        TokenStream ts = new TokenStream(new StringReader(""), null, 0);
+        TokenStream ts = new TokenStream(new StringReader("\\u0041 = y; x = 1 + 9; y = x++; y = this.a;"), "", 0);
 
         try {
-            assertTrue(reflectProperty(ts, "sourceString") == null);
-            assertTrue(reflectPropertyObject(ts, "sourceReader") != null);
-            assertTrue(reflectPropertyInt(ts, "sourceEnd") >= 0);
-            assertTrue(reflectPropertyInt(ts, "sourceCursor") == 0);
-            assertTrue(reflectPropertyInt(ts, "cursor") == 0);
-        } catch (Exception e) {
-            fail("Reflecting exceptions raised");
-        }
-
-        ts = new TokenStream(new StringReader("\\u0041 = y; x = 1 + 9; y = x++; y = this.a;"), "", 0);
-
-        try {
-            assertTrue(reflectProperty(ts, "sourceString") == null);
-            assertTrue(reflectPropertyObject(ts, "sourceReader") != null);
+            assertNull(reflectProperty(ts, "sourceString"));
+            assertNotNull(reflectPropertyObject(ts, "sourceReader"));
             assertTrue(reflectPropertyInt(ts, "sourceEnd") >= 0);
             assertTrue(reflectPropertyInt(ts, "sourceCursor") == 0);
             assertTrue(reflectPropertyInt(ts, "cursor") == 0);
@@ -135,10 +123,32 @@ public class TokenStreamTest {
     }
 
     @Test
-    public void testStringToKeyword() {
+    public void testTokenStreamNullStringEmpty() {
+        // Tests the constructor function without .js
+        // Input: empty sourceReader, null sourceString
+        // Expected output:
+        // (this.sourceString == null ==> this.sourceReader != null)
+        // (this.sourceEnd >= 0)
+        // (this.sourceCursor == 0 && this.cursor == 0);
+
+        TokenStream ts = new TokenStream(new StringReader(""), null, 0);
+
+        try {
+            assertNull(reflectProperty(ts, "sourceString"));
+            assertNotNull(reflectPropertyObject(ts, "sourceReader"));
+            assertTrue(reflectPropertyInt(ts, "sourceEnd") >= 0);
+            assertTrue(reflectPropertyInt(ts, "sourceCursor") == 0);
+            assertTrue(reflectPropertyInt(ts, "cursor") == 0);
+        } catch (Exception e) {
+            fail("Reflecting exceptions raised");
+        }
+    }
+
+    @Test
+    public void testStringToKeywordValid() {
         // Tests if the stringToKeyword function correctly maps strings to their keyword values
-        // Input: name = else, false, function, if, return, true, gibberish, (none)
-        // Expected output: 113, 44, 109, 112, 4, 45, 0, 0
+        // Input: name = else, false, function, if, return, true
+        // Expected output: 113, 44, 109, 112, 4, 45
 
         TokenStream ts = new TokenStream(null, "", 0);
         String[] keywords = {"else", "false", "function", "if", "return", "true"};
@@ -151,7 +161,15 @@ public class TokenStreamTest {
                 fail("Reflecting exceptions raised");
             }
         }
+    }
 
+    @Test
+    public void testStringToKeywordInvalid() {
+        // Tests if the stringToKeyword function correctly maps non-keyword values to 0
+        // Input: name = gibberish, (none)
+        // Expected output: 0, 0
+
+        TokenStream ts = new TokenStream(null, "", 0);
         String[] strings = {"gibberish", ""};
 
         for (int i = 0; i < strings.length; i++) {
@@ -164,19 +182,41 @@ public class TokenStreamTest {
     }
 
     @Test
-    public void testGetToken() {
-        // Tests if getToken gives a correct result
-// Input: non-null stringBuffer, null sourceString, non-null sourceBuffer and sourceReader
+    public void testGetTokenJS() {
+        // Tests if getToken gives a correct result when given JS
+        // Input: non-null stringBuffer, null sourceString, non-null sourceBuffer and sourceReader
+        // Expected output: -1 <= result <= 162
+        // Blackbox
+
+        TokenStream ts = new TokenStream(new StringReader("\\u0041 = y; x = 1 + 9; y = x++; y = this.a;"), null, 0);
+
+        try {
+            assertNotNull(reflectPropertyObject(ts, "stringBuffer"));
+            assertNull(reflectProperty(ts, "sourceString"));
+            assertNotNull(reflectPropertyObject(ts, "sourceBuffer"));
+            assertNotNull(reflectPropertyObject(ts, "sourceReader"));
+            assertTrue(ts.getToken() >= -1);
+            assertTrue(ts.getToken() <= 162);
+        } catch (Exception e) {
+            fail("Reflecting exceptions raised");
+        }
+    }
+
+    @Test
+    public void testGetTokenEmpty() {
+        // Tests if getToken gives a correct result without JS
+        // Input: non-null stringBuffer, null sourceString, non-null sourceBuffer and sourceReader
         // Expected output: -1 <= result <= 162
         // Blackbox
 
         TokenStream ts = new TokenStream(new StringReader(""), null, 0);
 
         try {
-            assertTrue(reflectPropertyObject(ts, "stringBuffer") != null);
-            assertTrue(reflectProperty(ts, "sourceString") == null);
-            assertTrue(reflectPropertyObject(ts, "sourceBuffer") != null);
-            assertTrue(reflectPropertyObject(ts, "sourceReader") != null);
+            assertNotNull(reflectPropertyObject(ts, "stringBuffer"));
+            assertNull(reflectProperty(ts, "sourceString"));
+            assertNotNull(reflectPropertyObject(ts, "sourceBuffer"));
+            assertNotNull(reflectPropertyObject(ts, "sourceReader"));
+            assertTrue(ts.getToken() >= -1);
             assertTrue(ts.getToken() <= 162);
         } catch (Exception e) {
             fail("Reflecting exceptions raised");
@@ -186,7 +226,7 @@ public class TokenStreamTest {
     @Test
     public void testGetTokenBadResult() {
         // Tests if getToken gives a bad result
-// Input: non-null stringBuffer, null sourceString, non-null sourceBuffer and sourceReader
+// Input: null stringBuffer, null sourceString, non-null sourceBuffer and sourceReader
         // Expected output: result < -1, result > 162
         // Blackbox
 
@@ -209,23 +249,91 @@ public class TokenStreamTest {
         }
     }
 
+//todo separate....
     @Test
-    public void testStringToNumber() {
-        // Tests
-        // Input:
-        // Expected output:
+    public void testStringToNumberValid() {
+        // Tests if stringToNumber get the right inputs;
+        // Input: non-null s, start = 0, 1, 3, radix = 10, 8, 16
+        // Expected output: a number of type double
 
-        try {
-        } catch (Exception e) {
-            fail("Reflecting exceptions raised");
+        TokenStream ts = new TokenStream(null, "", 0);
+        String[] string = {"0", "1", "13", "a"};
+        int[] radix = {10, 8, 16};
+        int[] start = {0, 1, 2};
+
+        for (int i = 0; i < radix.length; i++) {
+            try {
+                assertFalse(Double.isNaN(TokenStream.stringToNumber("0", 0, radix[i])));
+            } catch (Exception e) {
+                fail("Giving NaN when is a number");
+            }
+        }
+
+        for (int i = 0; i < radix.length; i++) {
+            try {
+                assertFalse(Double.isNaN(TokenStream.stringToNumber("15", 1, radix[i])));
+            } catch (Exception e) {
+                fail("Giving NaN when is a number");
+            }
+        }
+
+        for (int i = 0; i < string.length; i++) {
+            try {
+                assertFalse(Double.isNaN(TokenStream.stringToNumber(string[i], 0, 16)));
+            } catch (Exception e) {
+                fail("Giving NaN when is a number");
+            }
+        }
+
+        for (int i = 0; i < start.length; i++) {
+            try {
+                assertFalse(Double.isNaN(TokenStream.stringToNumber("164", start[i], 10)));
+            } catch (Exception e) {
+                fail("Giving NaN when is a number");
+            }
         }
     }
 
     @Test
+    public void testStringToNumberInvalid() {
+        // Tests if stringToNumber handles bad inputs;
+        // Input: null s, start < 0, radix != 10, 8, 16
+    // Expected output: ......todo
+        // Glassbox
+
+        TokenStream ts = new TokenStream(null, "", 0);
+
+        try {
+            // null s
+            TokenStream.stringToNumber(null, 1, 10);
+        } catch (NullPointerException e) {
+            // We want this
+        }
+
+        try {
+            // start < 0
+            TokenStream.stringToNumber("123", -1, 16);
+        } catch (StringIndexOutOfBoundsException e) {
+            // We want this
+        }
+
+        try {
+            // radix != 10, 8, 16 ==> NaN
+            assertTrue(Double.isNaN(TokenStream.stringToNumber("a", 1, 8)));
+            assertTrue(Double.isNaN(TokenStream.stringToNumber("1", 1, 10)));
+        } catch (Exception e) {
+            fail("Not getting NaN");
+        }
+    }
+
+    //@ ensures c <= 127 ==> \result == (c == 0x20 || c == 0x9 || c == 0xC || c == 0xB);
+    //public static boolean isJSSpace(int c)
+    @Test
     public void testIsJSSpace() {
-        // Tests
-        // Input:
-        // Expected output:
+        // Tests if isJSSpace handles whitespace characters (todo and non-breaking space?)
+        // Input: c <= 127
+        // Expected output: 0x20, 0x9, 0xC, 0xB
+        // todo: another version for c > 127 ==> 0xA0
 
         try {
         } catch (Exception e) {
