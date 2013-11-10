4fb7cdf921ad9f015e3f07bc1e351f0e113f339e
GSathish <sathish@gmail.com>
2013-11-10 00:40:58 -0800

    Update ASTNode.java

--- src/ca/ubc/ece/eece210/mp3/ast/ASTNode.java
+++ src/ca/ubc/ece/eece210/mp3/ast/ASTNode.java
@@ -16,25 +16,49 @@ public abstract class ASTNode {
     // variable.
     protected String arguments = null;
 
+    /**
+     * This is the default constructor for ASTNode.
+     * Creates a node given a token.
+     */
     public ASTNode(Token token) {
 	children = new ArrayList<ASTNode>(2);
 	this.token = token;
     }
 
+    /**
+     * This method sets the arguments for the node.
+     * @param arguments: a String that represents the arguments
+     */
     public void setArguments(String arguments) {
 	this.arguments = arguments;
     }
 
+    /**
+     * Add a child node to the ASTNode.
+     * @param node that is to be added as a child node.
+     */
     public void addChild(ASTNode node) {
 	children.add(node);
     }
 
+    /**
+     * Obtain the text associated with this node's token.
+     * @return payload string that is associated with the token.
+     */
     public String getText() {
 	return token.getPayload();
     }
 
+    /**
+     * This abstract method needs to be implemented by 
+     * any concrete classes that extend this abstract class.
+     */ 
     public abstract Set<Element> interpret(Catalogue argument);
 
+    /**
+     * This method returns a string representation of the ASTNode.
+     * @return string representation of ASTNode.
+     */
     @Override
     public String toString() {
 	if (children == null || children.size() == 0) {
