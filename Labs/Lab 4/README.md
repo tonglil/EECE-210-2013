3e25d163e62febb78aa6c19edeb342bda7c1c68a
Tony Li <tonyli.lives@gmail.com>
2013-11-10 22:55:57 -0800

    completed lab 4

---
+++ Labs/Lab 4/README.md
@@ -0,0 +1,7 @@
+GO!
+===
+
+From the root directory:
+`java -cp ./src/ com.concurrent.ConcurrentProg`
+
+Source: http://stackoverflow.com/questions/8027670/compiling-four-java-files-within-one-package-using-javac
---
+++ Labs/Lab 4/dir/src/com/concurrent/ConcurrentProg.java
@@ -0,0 +1,34 @@
+package com.concurrent;
+
+public class ConcurrentProg {
+    public static int tagCounter = 0;
+    public static Object tagCounterLock = new Object();
+
+    public static void main(String[] args) {
+        ConcurrentProg cp = new ConcurrentProg();
+        for (int i = 0; i < 4; i++) {
+            new Thread(cp.new PrintMsg()).start();
+        }
+
+    }
+
+    private static int incBy100000() {
+        for (int i = 0; i < 100000; i++) {
+            tagCounter++;
+        }
+
+        return tagCounter;
+    }
+
+    private class PrintMsg implements Runnable {
+        @Override
+        public void run() {
+            int tag;
+            synchronized (tagCounterLock) {
+                tag = incBy100000();
+
+            }
+            System.out.println("This is thread " + tag);
+        }
+    }
+}
